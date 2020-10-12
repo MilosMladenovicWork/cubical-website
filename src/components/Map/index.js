8ae0a87440bc3bc862258b1f884eb41451000575
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-10-12 18:24:01 +0200

    fixed bug with leaflet marker Object(...) is not a function

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import {Map as LeafletMap, Marker, TileLayer} from 'react-leaflet'
+import {Map, Marker, TileLayer} from 'react-leaflet'
 import {icon} from 'leaflet'
 
 import styles from './map.module.scss'
-import mapPin from '../../img/location.svg'
+import iconUrl from '../../img/location.svg'
 
-const Map = ({coords, zoom}) => {
+const CubicalMap = ({coords, zoom}) => {
     return(
         <div className={styles.map}>
-            <LeafletMap center={coords} zoom={zoom}>
+            <Map center={coords} zoom={zoom}>
                 <TileLayer
                     url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
                     attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
                 />
-                <Marker position={coords} icon={icon({iconUrl: mapPin, iconSize: [50, 50]})}/>
-            </LeafletMap>
+                {
+                    typeof window != 'undefined' &&
+                        <Marker position={coords} icon={icon({iconUrl, iconSize: [50, 50]})}/>
+                }
+            </Map>
         </div>
     )
 }
 
-export default Map
+export default CubicalMap
