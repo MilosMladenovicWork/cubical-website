08961c96af8491197fa7621f314e78665ae23e95
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-10-12 18:35:20 +0200

    changed map tile layer bcs of not free previous layer

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -10,8 +10,9 @@ const CubicalMap = ({coords, zoom}) => {
         <div className={styles.map}>
             <Map center={coords} zoom={zoom}>
                 <TileLayer
-                    url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
-                    attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                    subdomains={'abcd'}
                 />
                 {
                     typeof window != 'undefined' &&
