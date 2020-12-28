9c5347bf75d61425b255ab2c077124e7e8f07f83
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-12-28 12:58:19 +0100

    added contact form placeholder text in German

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -21,11 +21,11 @@ const ContactForm = ({children}) => {
                         <animated.div style={{...transformChildren, boxShadow: '0px 0px 0px rgba(0, 0, 0, 0)'}} className={styles.childContainer}>
                             {children}
                         </animated.div>
-                        <TextInputFieldAnimated style={transformChildren} placeholder='Surname'/> 
-                        <TextInputFieldAnimated style={transformChildren} placeholder='E-mail' type='email'/>
-                        <TextInputFieldAnimated style={transformChildren} placeholder='Phone' />
-                        <TextInputFieldAnimated style={transformChildren} placeholder='Desired call time' />
-                        <TextareaFieldAnimated style={transformChildren} placeholder='Your request'/>
+                        <TextInputFieldAnimated style={transformChildren} placeholder='Name'/> 
+                        <TextInputFieldAnimated style={transformChildren} placeholder='E-Mail' type='email'/>
+                        <TextInputFieldAnimated style={transformChildren} placeholder='Telefonnummer' />
+                        <TextInputFieldAnimated style={transformChildren} placeholder='Gewünschte Anrufzeit' />
+                        <TextareaFieldAnimated style={transformChildren} placeholder='Ihre Nachricht'/>
                         <animated.button style={{...transformChildren, boxShadow: '0px 0px 0px rgba(0, 0, 0, 0)'}} className={styles.sendButton}>
                             <img src={sendBlueImg} alt='send'/>
                         </animated.button>
