032270b028694bb9ae3f05c5a30e19d3c4c728b0
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2021-01-05 16:05:04 +0100

    made client changes to page intro element

--- src/components/PageIntro/index.js
+++ src/components/PageIntro/index.js
@@ -9,17 +9,19 @@ const PageIntro = ({data, children}) => {
     if(data){
         return(
             <Section>
-                {data.primary.heading &&
-                    <AppearOnViewContainer>
-                        <div dangerouslySetInnerHTML={{__html:data.primary.heading.html}}></div>
-                    </AppearOnViewContainer>
-                }
-                {data.primary.body1 && 
-                    <AppearOnViewContainer>
-                        <div className={styles.introText} dangerouslySetInnerHTML={{__html:data.primary.body1.html}}>
-                        </div>
-                    </AppearOnViewContainer>
-                }
+                <div className={styles.contentRow}>
+                    {data.primary.heading &&
+                        <AppearOnViewContainer>
+                            <div className={styles.heading} dangerouslySetInnerHTML={{__html:data.primary.heading.html}}></div>
+                        </AppearOnViewContainer>
+                    }
+                    {data.primary.body1 && 
+                        <AppearOnViewContainer>
+                            <div className={styles.introText} dangerouslySetInnerHTML={{__html:data.primary.body1.html}}>
+                            </div>
+                        </AppearOnViewContainer>
+                    }
+                </div>
             </Section>
         )
     }else{
