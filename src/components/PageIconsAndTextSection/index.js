f6b0ba17d7dff6e80524f8dbdea6a04cab0c7e3f
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2021-02-27 18:02:51 +0100

    added animation on view for iconsandtext component

--- src/components/PageIconsAndTextSection/index.js
+++ src/components/PageIconsAndTextSection/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'gatsby-image'
 
 import styles from './page-icons-and-text-section.module.scss'
+import AppearOnViewContainer from '../AppearOnViewContainer'
 
 const PageIconsAndTextSection = ({data}) => {
 
@@ -9,36 +10,39 @@ const PageIconsAndTextSection = ({data}) => {
 
   return(
     data &&
-    <div className={styles.container} id={(data && data.primary && data.primary.section_id) ? data.primary.section_id : ''}>
-        {
-          data.primary &&
-          data.primary.above_icons_text &&
-          data.primary.above_icons_text.html &&
-          <div className={styles.aboveIconsContainer} dangerouslySetInnerHTML={{__html: data.primary.above_icons_text.html}}>
-          </div>
-        }
-      <div className={styles.iconsContainer}>
-        {
-          data.items &&
-          data.items.map(item => {
-            return <div className={styles.iconCard}>
-              {
-                item.icon1 &&
-                item.icon1.localFile &&
-                item.icon1.localFile.childImageSharp &&
-                item.icon1.localFile.childImageSharp.fixed &&
-                <Image fixed={item.icon1.localFile.childImageSharp.fixed} alt={item.icon1.alt}/>
-              }
-              {
-                item.below_icon_text &&
-                item.below_icon_text.html &&
-                <div className={styles.cardContent} dangerouslySetInnerHTML={{__html:item.below_icon_text.html}}></div>
-              }
+    <AppearOnViewContainer>
+
+      <div className={styles.container} id={(data && data.primary && data.primary.section_id) ? data.primary.section_id : ''}>
+          {
+            data.primary &&
+            data.primary.above_icons_text &&
+            data.primary.above_icons_text.html &&
+            <div className={styles.aboveIconsContainer} dangerouslySetInnerHTML={{__html: data.primary.above_icons_text.html}}>
             </div>
-          })
-        }
+          }
+        <div className={styles.iconsContainer}>
+          {
+            data.items &&
+            data.items.map(item => {
+              return <div className={styles.iconCard}>
+                {
+                  item.icon1 &&
+                  item.icon1.localFile &&
+                  item.icon1.localFile.childImageSharp &&
+                  item.icon1.localFile.childImageSharp.fixed &&
+                  <Image fixed={item.icon1.localFile.childImageSharp.fixed} alt={item.icon1.alt}/>
+                }
+                {
+                  item.below_icon_text &&
+                  item.below_icon_text.html &&
+                  <div className={styles.cardContent} dangerouslySetInnerHTML={{__html:item.below_icon_text.html}}></div>
+                }
+              </div>
+            })
+          }
+        </div>
       </div>
-    </div>
+    </AppearOnViewContainer>
   )
 }
 
